c480c4753c905eacfa4ed829b2bbe96a0d70f30d
sebastien cossin <cossin.sebastien@gmail.com>
2023-02-10 21:55:02 -0300

    update to version 0.2.0

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -24,7 +24,7 @@ copyright = "2022, Sebastien Cossin"
 author = "Sebastien Cossin"
 
 # The full version, including alpha/beta/rc tags
-release = "0.1.1"
+release = "0.2.0"
 
 # -- General configuration ---------------------------------------------------
 
